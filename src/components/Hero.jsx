81a61d53b90fbb8809311f4d5774f76726e805e9
pavix0xd <minindupavith20@gmail.com>
2025-04-15 18:48:50 +0530

    hero animationssss

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import Button from "./Button.jsx";
 import {TiLocation, TiLocationArrow} from "react-icons/ti";
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
+
+import {ScrollTrigger} from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
 
@@ -25,6 +29,12 @@ const Hero = () => {
 
         setCurrentIndex(upcomingVideoIndex);
     }
+
+    useEffect(() => {
+        if(loadedVideos === totalVideos -1) {
+           setIsLoading(false);
+        }
+    })
 
     useGSAP(() => {
         if (hasClicked) {
@@ -49,10 +59,39 @@ const Hero = () => {
         }
     }, {dependencies: [currentIndex], revertOnUpdate: true})
 
+    useGSAP(() => {
+        gsap.set('#video-frame', {
+            clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+            borderRadius: '0 0 40% 10%',
+        })
+
+        gsap.from('#video-frame', {
+            clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+            borderRadius: '0 0 0 0',
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '#video-frame',
+                start: 'center center',
+                end: 'bottom center',
+                scrub: true,
+            }
+        })
+    })
+
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
     return (
         <div className="relative h-dvh w-screen overflow-x-hidden">
+
+            {isLoading && (
+                <div className= "flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+                    <div className="three-body">
+                        <div className="three-body__dot"/>
+                        <div className="three-body__dot"/>
+                        <div className="three-body__dot"/>
+                    </div>
+                </div>
+            )}
             <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-x-hidden rounded-lg bg-blue-75">
                 <div>
                     <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
@@ -90,7 +129,7 @@ const Hero = () => {
                 </div>
 
                 <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
-                    <b>Gaming</b>
+                    <b>Frontend <br/> Developer</b>
                 </h1>
 
                 <div className="absolute left-0 top-0 z-40 size-full">
@@ -117,7 +156,7 @@ const Hero = () => {
             </div>
 
             <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
-                <b>Gaming</b>
+                <b>Frontend <br/>Developer</b>
             </h1>
 
         </div>
