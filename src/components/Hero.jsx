d2335fbbb9be9f2d7e2909d2fec16f9f120d241f
pavix0xd <minindupavith20@gmail.com>
2025-04-17 21:42:15 +0530

    heroabout animate

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,48 +36,50 @@ const Hero = () => {
         }
     })
 
-    useGSAP(() => {
-        if (hasClicked) {
-            gsap.set('#next-video', {visibility: 'visible'});
-
-            gsap.to('#next-video', {
-                transformOrigin: 'center center',
-                scale: 1,
-                width: '100%',
-                height: '100%',
-                duration: 1,
-                ease: 'power1.inOut',
-                onStart:() => nextVideoRef.current.play(),
-            })
-
-            gsap.from('#current-video', {
-                transformOrigin: 'center center',
-                scale: 0,
-                duration: 1.5,
-                ease: 'power1.inOut',
-            })
-        }
-    }, {dependencies: [currentIndex], revertOnUpdate: true})
-
-    useGSAP(() => {
-        gsap.set('#video-frame', {
-            clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
-            borderRadius: '0 0 40% 10%',
-        })
-
-        gsap.from('#video-frame', {
-            clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
-            borderRadius: '0 0 0 0',
-            ease: 'power1.inOut',
-            scrollTrigger: {
-                trigger: '#video-frame',
-                start: 'center center',
-                end: 'bottom center',
-                scrub: true,
+    useGSAP(
+        () => {
+            if (hasClicked) {
+                gsap.set("#next-video", { visibility: "visible" });
+                gsap.to("#next-video", {
+                    transformOrigin: "center center",
+                    scale: 1,
+                    width: "100%",
+                    height: "100%",
+                    duration: 1,
+                    ease: "power1.inOut",
+                    onStart: () => nextVideoRef.current.play(),
+                });
+                gsap.from("#current-video", {
+                    transformOrigin: "center center",
+                    scale: 0,
+                    duration: 1.5,
+                    ease: "power1.inOut",
+                });
             }
-        })
-    })
+        },
+        {
+            dependencies: [currentIndex],
+            revertOnUpdate: true,
+        }
+    );
 
+    useGSAP(() => {
+        gsap.set("#video-frame", {
+            clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+            borderRadius: "0% 0% 40% 10%",
+        });
+        gsap.from("#video-frame", {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            borderRadius: "0% 0% 0% 0%",
+            ease: "power1.inOut",
+            scrollTrigger: {
+                trigger: "#video-frame",
+                start: "center center",
+                end: "bottom center",
+                scrub: true,
+            },
+        });
+    });
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
     return (
