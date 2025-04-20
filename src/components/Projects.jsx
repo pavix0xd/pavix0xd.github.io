afe2f9d688b7343b6a511c2e569edc39fc0c4e69
pavix0xd <minindupavith20@gmail.com>
2025-04-20 13:24:23 +0530

    feat: update Navbar with new navigation item and enhance Projects section with an ID

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -129,7 +129,7 @@ export const BentoCard = ({ src, title, description, isComingSoon, isVisit, butt
 };
 
 const Projects = () => (
-  <section className="bg-black pb-52">
+  <section id = "projects" className="bg-black pb-52">
     <div className="container mx-auto px-3 md:px-10">
       <div className="px-5 py-32">
         <p className="font-circular-web text-lg text-blue-50">
